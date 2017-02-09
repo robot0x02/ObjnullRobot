b0305c2553b2f7e2a399ff313f2a3c02c401ef4b
CodeFarmer <631320085@163.com>
2017-02-09 15:21:32 +0800

    完成第一版

--- src/GithubRobot/GithubRobot/Program.cs
+++ src/GithubRobot/GithubRobot/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Configuration;
 using System.Data;
+using System.Threading;
+using System.Threading.Tasks;
 using Octokit;
 using MySql.Data.MySqlClient;
 
@@ -15,7 +17,7 @@ namespace GithubRobot
         {
             Console.WriteLine("选择数据库：1.local 2.objnull");
             string constr = Console.ReadLine();
-            if(constr == "2")
+            if (constr == "2")
             {
                 MySQLDB.Conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["ObjNullConnection"].ConnectionString);
             }
@@ -23,40 +25,26 @@ namespace GithubRobot
             {
                 MySQLDB.Conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["LocalConnection"].ConnectionString);
             }
-            Console.WriteLine("命令：1.FollowBySQL 2.Start 3.Stop");
-            switch (Console.ReadLine())
+
+            while (true)
             {
-                case "1":
-                    FollowBySQL();
-                    break;
+                Console.WriteLine("执行命令：1.FollowBySQL 2.StartReadEvents 3.CheckAPIStatus");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        FollowBySQL();
+                        break;
+                    case "2":
+                        StartReadEvents();
+                        break;
+                    case "3":
+                        CheckAPIStatus();
+                        break;
+                }
             }
-            Console.ReadLine();
         }
-
-        public void temp()
-        {
-            GitHubClient github = new GitHubClient(new ProductHeaderValue("MyAmazingApp"));
-            github.Credentials = new Credentials("2fee142d6d760e6de72e0d81594607b54de62ae7");
-
-            github.User.Get("robot0x01");
-
-            github.User.Followers.Follow("631320085");
-
-            //IReadOnlyList<Activity> acts = github.Activity.Events.GetAllUserReceived("631320085").Result;
-            //foreach(Activity act in acts)
-            //{
-            //    Console.WriteLine(act.Actor.Login + " " + act.CreatedAt + " " + act.Repo.Name + " " + act.Type + " " + act.Payload.Sender);
-            //}
-
-            //ApiInfo apiInfo = github.GetLastApiInfo();
-            //// If the ApiInfo isn't null, there will be a property called RateLimit
-            //RateLimit rateLimit = apiInfo?.RateLimit;
-            //int? howManyRequestsCanIMakePerHour = rateLimit?.Limit;
-            //int? howManyRequestsDoIHaveLeft = rateLimit?.Remaining;
-            //DateTimeOffset? whenDoesTheLimitReset = rateLimit?.Reset;
-            //Console.WriteLine("Can:" + howManyRequestsCanIMakePerHour + " Left:" + howManyRequestsDoIHaveLeft + " Reset:" + whenDoesTheLimitReset);
-        }
-
+        
+        //用指定机器人关注指定查询的用户
         public static void FollowBySQL()
         {
             Console.WriteLine("输入SQL：");
@@ -76,6 +64,90 @@ namespace GithubRobot
                 github.User.Followers.Follow(row[0].ToString());
             }
             Console.WriteLine("关注完成。");
+        }
+
+        //抓取最新事件
+        public static void StartReadEvents()
+        {
+            Console.WriteLine("输入更新间隔分钟数：");
+            int waitmi = int.Parse(Console.ReadLine());
+            Console.WriteLine("更新程序已启动，输入任意字符停止。");
+            bool stop = false;
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    if(stop)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("更新中……");
+                    int count = 0;
+                    //获取每个机器账号的新事件
+                    string sql = "select GitHubLogin, GitHubAccessToken from nulluser where Email like '%objnull.com' and GitHubLogin like 'robot%'";
+                    foreach (DataRow row in MySQLDB.Query(sql).Rows)
+                    {
+                        string robot = row[0].ToString();
+
+                        //当前机器人最后更新时间
+                        string ledSql = "select CreateDate from githubevent where Robot='" + robot + "' order by CreateDate desc limit 1";
+                        DataTable ledDT = MySQLDB.Query(ledSql);
+                        DateTime LastEventDate;
+                        if (ledDT.Rows.Count == 0)
+                        {
+                            LastEventDate = DateTime.Now.AddYears(-1);
+                        }
+                        else
+                        {
+                            LastEventDate = DateTime.Parse(ledDT.Rows[0][0].ToString());
+                        }
+
+                        GitHubClient github = new GitHubClient(new ProductHeaderValue("objnulldotcom"));
+                        github.Credentials = new Credentials(row[1].ToString());
+                        IReadOnlyList<Activity> acts = github.Activity.Events.GetAllUserReceived(robot).Result;
+                        //插入最新事件
+                        foreach (Activity act in acts.Where(a => a.CreatedAt.LocalDateTime > LastEventDate))
+                        {
+                            string id = act.Id;
+                            string githubLogin = act.Actor.Login;
+                            string avatarUrl = act.Actor.AvatarUrl;
+                            DateTime createDate = act.CreatedAt.LocalDateTime;
+                            string eventType = act.Type;
+                            string repoName = act.Repo.Name;
+                            string addSql = "insert into githubevent values('" + id + "','" + githubLogin + "','" + avatarUrl +
+                                "',DATE_FORMAT('" + createDate.ToString("yyyy-MM-dd HH:mm:ss") + "','%Y-%m-%d %H:%i:%s'),'" + eventType + "','" + repoName + "','" + robot + "')";
+                            MySQLDB.Execute(addSql);
+                            count++;
+                        }
+                    }
+                    Console.WriteLine("更新完成，插入" + count + "条数据。下一次更新在" + DateTime.Now.AddMinutes(waitmi).ToString());
+                    Thread.Sleep(new TimeSpan(0, waitmi, 0));
+                }
+                Console.WriteLine("更新停止");
+            });
+            Console.ReadLine();
+            stop = true;
+        }
+
+        //查看API状态
+        public static void CheckAPIStatus()
+        {
+            string sql = "select GitHubLogin, GitHubAccessToken from nulluser where Email like '%objnull.com' and GitHubLogin like 'robot%'";
+            foreach (DataRow row in MySQLDB.Query(sql).Rows)
+            {
+                string robot = row[0].ToString();
+
+                GitHubClient github = new GitHubClient(new ProductHeaderValue("objnulldotcom"));
+                github.Credentials = new Credentials(row[1].ToString());
+                Console.WriteLine(github.User.Get(robot).Result.Login);
+                ApiInfo apiInfo = github.GetLastApiInfo();
+                // If the ApiInfo isn't null, there will be a property called RateLimit
+                RateLimit rateLimit = apiInfo?.RateLimit;
+                int? howManyRequestsCanIMakePerHour = rateLimit?.Limit;
+                int? howManyRequestsDoIHaveLeft = rateLimit?.Remaining;
+                DateTime? whenDoesTheLimitReset = rateLimit?.Reset.LocalDateTime;
+                Console.WriteLine("Can:" + howManyRequestsCanIMakePerHour + " Left:" + howManyRequestsDoIHaveLeft + " Reset:" + whenDoesTheLimitReset);
+            }
         }
     }
 }
